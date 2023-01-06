6a9ec5beff721fd65965be3056db8500c0d6e691
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-01-06 18:53:16 +0100

    Adds threading example

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import time
+import threading
+
 import paho.mqtt.client as mqtt
 
 
@@ -9,15 +12,27 @@ def on_connect(client, _, __, rc):
 
 def on_message(_, __, msg):
     print(msg.topic + " " + str(msg.payload))
+    thread = threading.Thread(None, rise, None, ('first', 1))
+    thread.start()
+    thread2 = threading.Thread(None, rise, None, ('second', 1))
+    thread2.start()
+
+
+def rise(entity):
+    count = 0
+    while count < 5:
+        time.sleep(0.5)
+        count += 1
+        print("%s: %s" % (entity, time.ctime(time.time())))
 
 
 def run_mqtt():
     client = mqtt.Client()
-    client.on_connect = on_connect
     client.on_message = on_message
     client.username_pw_set(os.getenv('MQTT_BROKER_USER'), os.getenv('MQTT_BROKER_PASSWORD'))
 
     client.connect(os.getenv('MQTT_BROKER_HOST'))
+    client.subscribe(os.getenv('MQTT_TOPIC'))
 
     client.loop_forever()
 
