8a2ccf274e469fc66b42b275ae7dd2c3598afb40
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-01-07 13:41:11 +0100

    Adds logging

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
+import sys
 import time
 import threading
 import json
+import logging
 
 import paho.mqtt.client as mqtt
 
 
 def on_message(client, _, __):
-    print("Message received.")
+    logging.info("Message received.")
     if threading.active_count() > 1:
-        print(f"Aborting, active thread count is {threading.active_count()}. The sun is already rising.")
+        logging.warning(f"Aborting, active thread count is {threading.active_count()}. The sun is already rising.")
         return
-    print("Starting sunrise.")
+    logging.info("Starting sunrise.")
     threading.Thread(None, rise_color, None, (client, 'Bedroom Right', 0, 5)).start()
     threading.Thread(None, rise_color, None, (client, 'Bedroom Left', 4 * 60, 3)).start()
     threading.Thread(None, rise_color, None, (client, 'Bedroom Back', 5 * 60, 3)).start()
+
+
+def on_connect(client, __, ___, rc, ____):
+    logging.info(f"Connected to mqtt broker. Return code: {rc}")
+
+    topic = os.getenv('MQTT_TOPIC')
+    logging.info(f"Subscribing to topic {topic}")
+    client.subscribe(topic)
 
 
 def rise_color(client, name, initial_delay=0, delay=4):
@@ -35,19 +45,43 @@ def rise_color(client, name, initial_delay=0, delay=4):
             "color_mode": "xy"
         }
         count += 1
-        client.publish(f'zigbee2mqtt/{name}/set', json.dumps(payload))
+
+        topic = f"zigbee2mqtt/{name}/set"
+        payload_serialized = json.dumps(payload)
+
+        logging.debug(f"Publishing to topic {topic}. Payload: {payload_serialized}")
+        client.publish(topic, payload_serialized)
         time.sleep(delay)
 
 
 def start_mqtt_client():
-    client = mqtt.Client()
+    client = mqtt.Client(protocol=mqtt.MQTTv5)
     client.on_message = on_message
+    client.on_connect = on_connect
+
+    client.on_subscribe = lambda _, __, ___, reason_codes, _____: logging.info(
+        f"Subscribed successfully. Reason codes: {', '.join([c.getName() for c in reason_codes])}")
+    client.on_connect_fail = lambda _, __: logging.info("Unable to connect to mqtt broker")
     client.username_pw_set(os.getenv('MQTT_BROKER_USER'), os.getenv('MQTT_BROKER_PASSWORD'))
-    client.connect(os.getenv('MQTT_BROKER_HOST'))
-    client.subscribe(os.getenv('MQTT_TOPIC'))
+
+    host = os.getenv('MQTT_BROKER_HOST')
+
+    logging.info(f"Connecting to {host}")
+    client.connect(host)
 
     client.loop_forever()
 
 
+def initialize_logging():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
 if __name__ == '__main__':
+    initialize_logging()
     start_mqtt_client()
